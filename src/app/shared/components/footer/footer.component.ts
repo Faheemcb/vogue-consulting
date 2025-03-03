804f77aaa6caec502ef2e058f2f8ae4be14c8457
Faheemcb <faheemmint978@gmail.com>
2025-03-03 23:46:13 +0530

    feat: ui changed

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-footer',
   template: `
   <div class="h-50"> 
-<div class="h-[4px] bg-white opacity-20 mx-auto my-12 max-w-screen-xl"></div>
+<div class="h-[4px] bg-white opacity-20 my-12 lg:ml-16 lg:w-[1350px]"></div>
 <footer class="py-4">
   <p class="text-white text-center text-sm sm:text-base lg:text-lg font-sans">
     © 2024 Vogue Consulting. All Rights Reserved.
