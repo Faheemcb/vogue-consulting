f3f15e97ce89d9f5536ebe973b71054aea249229
Faheemcb <faheemmint978@gmail.com>
2025-03-16 23:33:19 +0530

    feat: made footer responsive

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -3,13 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   template: `
-  <div class="h-50"> 
-<div class="h-[4px] bg-white opacity-20 my-12 lg:ml-16 lg:w-[1350px]"></div>
-<footer class="py-4">
-  <p class="text-white text-center text-sm sm:text-base lg:text-lg font-sans">
-    © 2024 Vogue Consulting. All Rights Reserved.
-  </p>
-</footer>
+  <div class="container mx-auto px-4"> 
+    <div class="h-[1px] sm:h-[2px] md:h-[3px] lg:h-[4px] bg-white opacity-20 my-6 sm:my-8 md:my-10 lg:my-12 w-full"></div>
+    <footer class="py-2 sm:py-3 md:py-4">
+      <p class="text-white text-center text-xs sm:text-sm md:text-base lg:text-lg font-sans">
+        © 2024 Vogue Consulting. All Rights Reserved.
+      </p>
+    </footer>
   </div>
   `,
   styles: [],
